f6f4be3bad98113ddf605eb6b38656aded62452c
Artem Lytkin <lytkinreal@mail.ru>
2018-02-20 08:04:13 +0300

    Delegates

--- BlueLibrary/Controller/ViewController.m
+++ BlueLibrary/Controller/ViewController.m
@@ -8,7 +8,15 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "LibraryAPI.h"
+#import "Album+TableRepresentation.h"
+
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate> {
+  UITableView *_dataTable;
+  NSArray *_allAlbums;
+  NSDictionary *_currentAlbumData;
+  int _currentAlbumIndex;
+}
 
 @end
 
@@ -17,15 +25,83 @@
 - (void)viewDidLoad
 {
   [super viewDidLoad];
+  // 1
+  self.view.backgroundColor = [UIColor colorWithRed:0.76f green:0.81f blue:0.87f alpha:1.f];
+  _currentAlbumIndex = 0;
   
-  NSLog( @"%@", [@(1) description]);
-  // Do any additional setup after loading the view, typically from a nib.
+  //2
+  _allAlbums = [[LibraryAPI sharedInstance] albums];
+  
+  // 3 UITableView
+  CGRect frame = CGRectMake(0, 120, self.view.frame.size.width, self.view.frame.size.height - 120);
+  _dataTable = [[UITableView alloc] initWithFrame:frame style:UITableViewStyleGrouped];
+  _dataTable.delegate = self;
+  _dataTable.dataSource = self;
+  _dataTable.backgroundView = nil;
+  [self.view addSubview:_dataTable];
+  
+  // 4
+  [self showDataForAlbumAtIndex:_currentAlbumIndex];
 }
 
-- (void)didReceiveMemoryWarning
-{
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+- (void)showDataForAlbumAtIndex:(int)albumIndex {
+  if (albumIndex < _allAlbums.count) {
+    Album *album = _allAlbums[albumIndex];
+    _currentAlbumData = [album tr_tableRepresentation];
+  } else {
+    _currentAlbumData = nil;
+  }
+  [_dataTable reloadData];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return [_currentAlbumData[@"titles"] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+  if (!cell) {
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+  }
+  
+  cell.textLabel.text = _currentAlbumData[@"titles"][indexPath.row];
+  cell.detailTextLabel.text = _currentAlbumData[@"values"][indexPath.row];
+  
+  return cell;
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
